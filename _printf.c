dc0a9b00ab4ace4155a904769fe4b2b394d21bac
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-27 16:05:43 +0000

    printf replaced

--- _printf.c
+++ _printf.c
@@ -2,59 +2,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+
 /**
- *_printf - a funtion that behaves like the stdio.h printf();
- *@format: points to a constant format list
- *Return: 0 or result
+ * _printf - a function that behaves like the stdio.h printf();
+ * @format: points to a constant format list
+ * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-	/*Variables*/
-	unsigned int i = 0, j = 0;
-	int count = 0;
-	print func_type[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_mod},
-		{'\0', NULL}
-	};
-	va_list args;
-	/*strar argument iteration*/
-	va_start(args, format);
-	if (format == NULL)
-	{
-		return (1);
-	}
-	else
-	{
-		while (format[i] != '\0')
-		{
-			j = 0;
-			if (format[i] == '%')
-			{
-				i++;
-				while (format[i] != *func_type[j].indi)
-				{
-					if (format[i] == *func_type[j].indi)
-					{
-						count += func_type[j].handler(args);
-						i++;
-						break;
-					}
-					j++;
-				} /*end inner while*/
-			} /*end if*/
-			else
-			{
-				write(1, &format[i], strlen(format));
-				count++;
-				return (count);
-			} /*end if else*/
-			i++;
-		} /*end inner while*/
-		/*Mark end or varidic and return count*/
-		va_end(args);
-		return (count);
-	} /*end if else for format*/
-} /*end function*/
+    /* Variables */
+    unsigned int i = 0, j = 0;
+    int printed_chars = 0; /** To keep track of the total characters printed */
+    print func_type[] = {
+        {"c", print_char},
+        {"s", print_string},
+        {"%", print_mod},
+        {"d", print_int},
+        {"i", print_int},
+        {NULL, NULL}};
+    va_list args;
 
+    /* Start argument iteration */
+    va_start(args, format);
+    while (format != NULL && format[i])
+    {
+        if (format[i] == '%')
+        {
+            j = 0;
+            while (j < 5)
+            {
+                if (format[i + 1] == *func_type[j].indi)
+                {
+                    func_type[j].handler(args);
+                    i++; /* Skip the next character when % is found */
+                    break;
+                }
+                j++;
+            }
+        }
+        else
+        {
+            _putchar(format[i]);
+            printed_chars++;
+            printf("%c", printed_chars);
+        }
+        i++;
+    }
+    va_end(args);
+    return (printed_chars);
+}
