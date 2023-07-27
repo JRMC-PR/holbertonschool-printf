61b72b8b54635d35bc75e3de4704d49613c5f5ca
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-27 19:53:49 +0000

    ./functions.c

--- functions.c
+++ functions.c
@@ -6,16 +6,16 @@
 /**
  * print_char - a function that prints a char
  * @args: argument of the list
+ *
  */
-
 int print_char(va_list args)
 {
-    /*variables*/
-    int count = 0;
+	/*variables*/
+	int count = 0;
 
-    _putchar(va_arg(args, int));
-    count++;
-    return (count);
+	_putchar(va_arg(args, int));
+	count++;
+	return (count);
 }
 
 /**
@@ -24,22 +24,22 @@ int print_char(va_list args)
  */
 int print_int(va_list args)
 {
-    /*Variables*/
-    int num = va_arg(args, int), count = 0;
-    char buffer[12]; /** store the int string, size of int buffer */
-    int printed_chars = 0;
+	/*Variables*/
+	int num = va_arg(args, int), count = 0;
+	char buffer[12]; /** store the int string, size of int buffer */
+	int printed_chars = 0;
+	int chars_written = sprintf(buffer, "%d", num);
 
-    int chars_written = sprintf(buffer, "%d", num);
-    if (chars_written > 0)
-    {
-        printed_chars += write(STDOUT_FILENO, buffer, chars_written);
-    }
+	if (chars_written > 0)
+	{
+		printed_chars += write(STDOUT_FILENO, buffer, chars_written);
+	}
 
-    if (printed_chars <= 0)
-    {
-        write(STDOUT_FILENO, "Conversion error", 16);
-    }
-    return (count);
+	if (printed_chars <= 0)
+	{
+		write(STDOUT_FILENO, "Conversion error", 16);
+	}
+	return (count);
 }
 
 /**
@@ -48,25 +48,29 @@ int print_int(va_list args)
  */
 int print_mod(va_list args)
 {
-    /*Variables*/
-    int count = 0;
-    char character = '%';
+	/*Variables*/
+	int count = 0;
+	char character = '%';
 
-    write(STDOUT_FILENO, &character, 1);
-    count++;
-    (void)args;
-    return (count);
+	write(STDOUT_FILENO, &character, 1);
+	count++;
+	(void)args;
+	return (count);
 }
 
+/**
+ * print_string - a function that prints a string
+ * @args: argument of the list
+ */
 int print_string(va_list args)
 {
-    /*Variales*/
-    char *string = va_arg(args, char *);
-    int count = 0;
+	/*Variales*/
+	char *string = va_arg(args, char *);
+	int count = 0;
 
-    if (string == NULL)
-        string = "(nil)";
-    write(STDOUT_FILENO, string, strlen(string));
-    count++;
-    return (count);
+	if (string == NULL)
+		string = "(nil)";
+	write(STDOUT_FILENO, string, strlen(string));
+	count++;
+	return (count);
 }
