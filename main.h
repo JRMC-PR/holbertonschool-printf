f474d270131f54e3a843a5b4b259bb2b6417f641
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-07-27 20:04:44 +0000

    error fix betty for main.h

--- main.h
+++ main.h
@@ -3,11 +3,15 @@
 
 #include <stdarg.h> /* Include the va_list type for variable arguments */
 
-/* Define the struct print */
+/**
+ * struct print_struct - ...
+ * @indi: ...
+ * @handler: ...
+ */
 typedef struct print_struct
 {
-    char *indi;
-    int (*handler)(va_list agrs);
+char *indi;
+int (*handler)(va_list agrs);
 } print;
 
 /* Function prototypes for _printf and additional functions */
@@ -17,6 +21,5 @@ int print_string(va_list args);
 int print_mod(va_list args);
 int print_int(va_list args);
 int _putchar(char c);
-
 #endif
 
