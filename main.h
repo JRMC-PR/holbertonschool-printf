7193c153be3c37672cf6668ca7eb076bdd1c6823
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-27 18:31:11 +0000

    ./main.h

--- main.h
+++ main.h
@@ -6,16 +6,16 @@
 /* Define the struct print */
 typedef struct print_struct
 {
-char *indi;
-void (*handler)(va_list agrs);
+    char *indi;
+    int (*handler)(va_list agrs);
 } print;
 
 /* Function prototypes for _printf and additional functions */
 int _printf(const char *format, ...);
-void print_char(va_list args);
-void print_string(va_list args);
-void print_mod(va_list args);
-void print_int(va_list args);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_mod(va_list args);
+int print_int(va_list args);
 int _putchar(char c);
 
 #endif
