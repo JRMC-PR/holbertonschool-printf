87b39cd9306e82f318c61b4d7c29ebee7d63119b
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-27 17:02:31 +0000

    ./_printf.c

--- _printf.c
+++ _printf.c
@@ -11,44 +11,43 @@
 
 int _printf(const char *format, ...)
 {
-    /* Variables */
-    unsigned int i = 0, j = 0;
-    int printed_chars = 0; /** To keep track of the total characters printed */
-    print func_type[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_mod},
-        {"d", print_int},
-        {"i", print_int},
-        {NULL, NULL}};
-    va_list args;
+	/* Variables */
+	unsigned int i = 0, j = 0;
+	int printed_chars = 0; /** To keep track of the total characters printed */
+	print func_type[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_mod},
+		{"d", print_int},
+		{"i", print_int},
+		{NULL, NULL}};
+	va_list args;
 
-    /* Start argument iteration */
-    va_start(args, format);
-    while (format != NULL && format[i])
-    {
-        if (format[i] == '%')
-        {
-            j = 0;
-            while (j < 5)
-            {
-                if (format[i + 1] == *func_type[j].indi)
-                {
-                    func_type[j].handler(args);
-                    i++; /* Skip the next character when % is found */
-                    break;
-                }
-                j++;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            printed_chars++;
-            printf("%c", printed_chars);
-        }
-        i++;
-    }
-    va_end(args);
-    return (printed_chars);
+	/* Start argument iteration */
+	va_start(args, format);
+	while (format != NULL && format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (j < 5)
+			{
+				if (format[i + 1] == *func_type[j].indi)
+				{
+					func_type[j].handler(args);
+					i++; /* Skip the next character when % is found */
+					break;
+				}
+				j++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			printed_chars++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (printed_chars);
 }
