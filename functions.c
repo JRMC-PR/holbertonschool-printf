d11579e861f8ae4274ccdce1afb49aa0a34167ce
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-07-27 18:24:47 +0000

    ./functions.c

--- functions.c
+++ functions.c
@@ -1,61 +1,72 @@
 #include "main.h"
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <string.h>
-
 
 /**
  * print_char - a function that prints a char
  * @args: argument of the list
  */
-void print_char(va_list args)
-{
-	_putchar(va_arg(args, int));
-}
 
+int print_char(va_list args)
+{
+    /*variables*/
+    int count = 0;
+
+    _putchar(va_arg(args, int));
+    count++;
+    return (count);
+}
 
 /**
  * print_int - a function that prints an integer
  * @args: argument of the list
  */
-void print_int(va_list args)
+int print_int(va_list args)
 {
-	int num = va_arg(args, int);
-	char buffer[12]; /** store the int string, size of int buffer */
-	int printed_chars = 0;
+    /*Variables*/
+    int num = va_arg(args, int), count = 0;
+    char buffer[12]; /** store the int string, size of int buffer */
+    int printed_chars = 0;
 
-	int chars_written = sprintf(buffer, "%d", num);
-	if (chars_written > 0)
-	{
-		printed_chars = write(STDOUT_FILENO, buffer, chars_written);
-	}
+    int chars_written = sprintf(buffer, "%d", num);
+    if (chars_written > 0)
+    {
+        printed_chars += write(STDOUT_FILENO, buffer, chars_written);
+    }
 
-	if (printed_chars <= 0)
-	{
-		write(STDOUT_FILENO, "Conversion error", 16);
-	}
-}
-
-/**
- * print_string - a function that prints a string
- * @args: argument of the list
- */
-void print_string(va_list args)
-{
-	char *string = va_arg(args, char *);
-	if (string == NULL)
-		string = "(nil)";
-	write(STDOUT_FILENO, string, strlen(string));
+    if (printed_chars <= 0)
+    {
+        write(STDOUT_FILENO, "Conversion error", 16);
+    }
+    return (count);
 }
 
 /**
  * print_mod - a function that prints a %
  * @args: argument of the list
  */
-void print_mod(va_list args)
+int print_mod(va_list args)
 {
+    /*Variables*/
+    int count = 0;
+    char character = '%';
 
-	char character = '%';
-	write(STDOUT_FILENO, &character, 1);
-	(void)args;
+    write(STDOUT_FILENO, &character, 1);
+    count++;
+    (void)args;
+    return (count);
+}
+
+int print_string(va_list args)
+{
+    /*Variales*/
+    char *string = va_arg(args, char *);
+    int count = 0;
+
+    if (string == NULL)
+        string = "(nil)";
+    write(STDOUT_FILENO, string, strlen(string));
+    count++;
+    return (count);
 }
